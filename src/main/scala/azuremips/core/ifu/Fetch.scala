aedc79985a10c075399e7c3427550830cbaabefa
cgruixiao <cgruixiao@outlook.com>
2022-08-01 09:47:11 +0800

    fix push/popen bug(50%)

--- src/main/scala/azuremips/core/ifu/Fetch.scala
+++ src/main/scala/azuremips/core/ifu/Fetch.scala
@@ -176,10 +176,10 @@ class Fetch extends Component {
     // stage2 Redirection block begin
     // ras
     val ras = Ras(depth = 16)
-    val rasRedirectEn = branchInfos(brInstIdx).isReturn && ras.io.topValid && 
+    val rasRedirectEn = branchInfos(brInstIdx).isReturn && ras.io.topValid && iCacheInstValids.orR && brInstIdx =/= U(3)
+    ras.io.pushEn   := branchInfos(brInstIdx).isCall && iCacheInstValids.orR && brInstIdx =/= U(3)
+    ras.io.popEn    := branchInfos(brInstIdx).isReturn && ras.io.topValid && 
                           iCacheInstValids.orR && brInstIdx =/= U(3)
-    ras.io.pushEn   := branchInfos(brInstIdx).isCall
-    ras.io.popEn    := branchInfos(brInstIdx).isReturn
     val rasPushPc = UInt(32 bits)
     switch (brInstIdx) {
       is (U(0)) { rasPushPc := instPcPkg(2) }
