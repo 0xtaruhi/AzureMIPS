a75efe41368d91b4daf0c89e8aee7d8dbee502a0
0xtaruhi <zhang_zhengyi@outlook.com>
2022-08-01 05:05:37 -0700

    delete bht & fix decoder(mul) bug

--- src/main/scala/azuremips/core/ifu/IFConfig.scala
+++ src/main/scala/azuremips/core/ifu/IFConfig.scala
@@ -6,15 +6,7 @@ import azuremips.core._
 
 case class IFConfig(
   instFetchWidth: Int = 128,
-  rasDepth: Int = 16,
-  rasCounterWidth: Int = 4,
-  fetchBufferDepth: Int = 8,
-  l0btbDepth: Int = 16
-
-  bhtUpperBound: Int = 10,
-  bhtLowerBound: Int = 2,
+  rasDepth: Int = 16
 ) {
   val instFetchNum = instFetchWidth / 32
-  val bhtAddrWidth = bhtUpperBound + 1 - bhtLowerBound
-  val bhtDepth = scala.math.pow(2, bhtAddrWidth).toInt
 }
