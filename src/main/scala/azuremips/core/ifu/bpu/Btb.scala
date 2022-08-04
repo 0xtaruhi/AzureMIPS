657b783897a21410c1382ba8c020c863de5f6dad
cgruixiao <cgruixiao@outlook.com>
2022-08-04 17:17:38 +0800

    2 port read btb

--- src/main/scala/azuremips/core/ifu/bpu/Btb.scala
+++ src/main/scala/azuremips/core/ifu/bpu/Btb.scala
@@ -6,35 +6,45 @@ import azuremips.core._
 
 case class Btb() extends Component with BtbConfig {
   val io = new Bundle {
-    val vaddr = in UInt(32 bits)
+    val vaddr1 = in UInt(32 bits)
+    val vaddr2 = in UInt(32 bits)
     val updatePc = in UInt(32 bits) // for addr
     val updateEn = in Bool()
     val actualTarget = in UInt(32 bits)
-    val btbHit = out Bool()
-    val predictTarget = out UInt(32 bits)
+    val btbHit1 = out Bool()
+    val btbHit2 = out Bool()
+    val predictTarget1 = out UInt(32 bits)
+    val predictTarget2 = out UInt(32 bits)
   }
 
   val btbRam = Mem(UInt(32 bits), btbSize)
   val tagRam = Mem(UInt(tagWidth bits), btbSize)
   val validRam = RegInit(U(0, btbSize bits))
 
-  val btb_addr = getBtbAddr(io.vaddr)
-  val vtag = getTag(io.vaddr)
+  val btb_addr1 = getBtbAddr(io.vaddr1)
+  val btb_addr2 = getBtbAddr(io.vaddr2)
+  val vtag1 = getTag(io.vaddr1)
+  val vtag2 = getTag(io.vaddr2)
   val refill_addr = getBtbAddr(io.updatePc)
   val refill_tag = getTag(io.updatePc)
   val refill_mask = Mux(io.updateEn, B"1", B"0")
   // tagRam & validRam
-  val tag = tagRam.readAsync(address=btb_addr)
+  val tag1 = tagRam(btb_addr1)
+  val tag2 = tagRam(btb_addr2)
   tagRam.write(address=refill_addr, data=refill_tag, enable=io.updateEn, mask=refill_mask)
-  val valid = validRam(btb_addr)
+  val valid1 = validRam(btb_addr1)
+  val valid2 = validRam(btb_addr2)
   when (io.updateEn) { validRam(refill_addr) := True }
 
   // dataRam
   btbRam.write(address=refill_addr, data=io.actualTarget, enable=io.updateEn, mask=refill_mask)
-  io.predictTarget := btbRam.readSync(address=btb_addr, enable=True)
+  io.predictTarget1 := btbRam.readSync(address=btb_addr1, enable=True)
+  io.predictTarget2 := btbRam.readSync(address=btb_addr2, enable=True)
 
-  val is_hit = RegNext(tag === vtag && valid) init(False)
-  io.btbHit := is_hit
+  val is_hit1 = RegNext(tag1 === vtag1 && valid1) init(False)
+  val is_hit2 = RegNext(tag2 === vtag2 && valid2) init(False)
+  io.btbHit1 := is_hit1
+  io.btbHit2 := is_hit2
 
   def getTag(vaddr: UInt): UInt = {
     vaddr(tagUpperBound downto tagLowerBound)
